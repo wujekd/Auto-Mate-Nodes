04ee7f4357459783b83c3b8102a8f38c33563fdf
Dominik Wujek <dom.wujek@gmail.com>
2025-02-02 18:23:25 +0000

    keyholder node design

--- Keyholder-Node/src/main.cpp
+++ Keyholder-Node/src/main.cpp
@@ -5,62 +5,85 @@
 #include <DallasTemperature.h>
 #include "secrets.h"
 
-#define SENSOR_PIN 21
-#define ONE_WIRE_BUS 4
-
+#define KEY_SENSOR_PIN 21
+#define TEMPERATURE_SENSOR_PIN 4
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
-const char* serverURL = SERVER_PORT;
+const char* serverURL = SERVER_IP;
 
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
+OneWire oneWireInstance(TEMPERATURE_SENSOR_PIN);
+DallasTemperature sensors(&oneWireInstance);
 
-int logCounter = 0;
-int serverKeyState = -1;
+int temperatureReadTimer = 0;
+int keySensorState = -1;
+int temperatureReadsSinceLastLogged = 0;
+float temperatureReads[12];
+
 
 void setup() {
-    Serial.begin(115200);
     WiFi.begin(ssid, password);
+    pinMode(TEMPERATURE_SENSOR_PIN, INPUT_PULLDOWN);
+    sensors.begin();
 
-    pinMode(SENSOR_PIN, INPUT);
-
-    sensors.begin();    
-
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(1000);
-        Serial.print(".");
-    }
-
+    // Serial.begin(115200);
+    // while (WiFi.status() != WL_CONNECTED) {
+    //     delay(1000);
+    //     Serial.print(".");
+    // }
 }
 
+
 void loop() {
-  
   if (WiFi.status() == WL_CONNECTED) {
 
-    sensors.requestTemperatures();  // Send command to get temperature
-    float temperature = sensors.getTempCByIndex(0);  // Read first sensor
-    int keystate = digitalRead(SENSOR_PIN);
+    // node logic
 
-    // if (keystate != serverKeyState){
-      String request = String(serverURL) + "/update?state=" + String(keystate) + "&count=" + String(logCounter) + "&temp=" + String(temperature);
-      HTTPClient http;
-      http.begin(request);
-      int responseCode = http.GET();
-      Serial.println(http.getString()); 
-      http.end();
-    // }
+    if (digitalRead(KEY_SENSOR_PIN) != keySensorState){
+      int keyChangeCount = 0;
+      do
+      {
+        keyChangeCount++;
+        if (keyChangeCount >= 250){
+          keySensorState = digitalRead(KEY_SENSOR_PIN);
+          HTTPClient http;
+          String request = String(serverURL) + "/update?state=" + String(keySensorState);
+          http.begin(request);
+          http.end();
+          break;
+        }
+        delay(1);
+      } while (digitalRead(KEY_SENSOR_PIN) != keySensorState);
       
-    logCounter++;
-    if (logCounter > 99){
-      logCounter = 0;
-    };
+    } else {
+      temperatureReadTimer++;
+      if (temperatureReadTimer >= 1000){
 
+        if (temperatureReadsSinceLastLogged >= 11){
+          //calculate average of all logs and emit new data to the server
+          float sum = 0;
+          for (int i=0; i < 11; i++){
+            sum += temperatureReads[i];
+          }
+          float averageTemperature = sum / 12;
+          HTTPClient http;
+          String request = String(serverURL) + "/update?temp=" + String(averageTemperature);
+          http.begin(request);
+          int responseCode = http.GET();
+          // Serial.println(http.getString()); 
+          http.end();
+          temperatureReadsSinceLastLogged = 0;
+
+        } else {
+          temperatureReads[temperatureReadsSinceLastLogged] = digitalRead(TEMPERATURE_SENSOR_PIN);
+          temperatureReadsSinceLastLogged++;
+        }
+      }
+    }
+    delay(10);
   } else {
     Serial.println("Disconnected! Reconnecting...");
     WiFi.disconnect();
     WiFi.reconnect();
   }
-  
-  delay(2000);
 }
